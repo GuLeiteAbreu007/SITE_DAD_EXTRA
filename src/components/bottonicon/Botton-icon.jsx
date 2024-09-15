eff5a028a646a06b0343aab9c9e5fa7aaaecb239
IRenatoBarros <renatofilho9722@gmail.com>
2024-09-15 13:08:37 -0300

    colocando um evento no  bottonicon

--- src/components/bottonicon/Botton-icon.jsx
+++ src/components/bottonicon/Botton-icon.jsx
@@ -1,8 +1,22 @@
 import style from './Botton-icon.module.css'
+import React, { useState } from 'react';
 
 function Bottonicon({srcIcon, desc}){
+
+    const [color, setColor] = useState('black')
+    const [background, setBackground] = useState('white')
+    
+    const mouseEnter = () => {
+        setBackground('#DB4444')
+        setColor('white')
+    }
+
+    const mouseLeave = () => {
+        setBackground('white')
+        setColor('black')
+    }
     return(
-        <div className={style.botton}>
+        <div className={style.botton} onMouseLeave={mouseLeave} onMouseEnter={mouseEnter} style={{backgroundColor: background, color:color}}>
             <img src={srcIcon}></img>
             <p>{desc}</p>
         </div>
