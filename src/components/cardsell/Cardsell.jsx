ffb8d9e236633534ea79624b65ee0750981642ff
IRenatoBarros <renatofilho9722@gmail.com>
2024-09-16 07:41:32 -0300

    alterando o componente time para o horário em tempo real

--- src/components/cardsell/Cardsell.jsx
+++ src/components/cardsell/Cardsell.jsx
@@ -1,8 +1,17 @@
 import style from './Cardsell.module.css'
 import fivestar from '../../assets/fivestar.png'
+import fourstar from '../../assets/fourstar.png'
 import React, { useState } from 'react';
 
 function Cardsell({imgSrc,alt,free,price,pricedel,title,numberstar}){
+    let img = ''
+    let group = ''
+    if(numberstar>75){
+        img = fivestar
+    }else{
+        img = fourstar
+    }
+    const className = free ? 'group' : 'empty-class';
     return(
         <div>
             <p className={style.free}>{free}</p>
@@ -10,13 +19,15 @@ function Cardsell({imgSrc,alt,free,price,pricedel,title,numberstar}){
                 <img src={imgSrc} alt={alt}></img>
             </div>
             <h1 className={style.title}>{title}</h1>
-            <div className={style.prices}>
-                <h1 className={style.pricee}>{price}</h1>
-                <del className={style.dell}>{pricedel}</del>
-            </div>
-            <div className={style.stars}>
-                <img src={fivestar} alt="cinco estrelas" />
-                <p>({numberstar})</p>
+            <div className={style.className}>
+                <div className={style.prices}>
+                    <h1 className={style.pricee}>{price}</h1>
+                    <del className={style.dell}>{pricedel}</del>
+                </div>
+                <div className={style.stars}>
+                    <img src={img} alt="cinco estrelas" />
+                    <p>({numberstar})</p>
+                </div>
             </div>
         </div>
     )
--- src/components/time/Time.jsx
+++ src/components/time/Time.jsx
@@ -1,25 +1,52 @@
 import style from "./Time.module.css"
+import React, { useState, useEffect } from 'react';
 function Time(){
+    const [time, setTime] = useState({
+        day: new Date().getDay(),
+        hours: new Date().getHours(),
+        minutes: new Date().getMinutes(),
+        seconds: new Date().getSeconds(),
+      });
+    
+      // Função que atualiza o tempo a cada segundo
+      useEffect(() => {
+        const interval = setInterval(() => {
+          const now = new Date();
+          setTime({
+            day: new Date().getDay(),
+            hours: now.getHours(),
+            minutes: now.getMinutes(),
+            seconds: now.getSeconds(),
+          });
+        }, 1000);
+    
+        return () => clearInterval(interval); // Limpa o intervalo ao desmontar o componente
+      }, []);
+    
+      // Formatar a hora para sempre ter dois dígitos
+      const formatTime = (unit) => {
+        return unit < 10 ? `0${unit}` : unit;
+      };
     return(
         <div className={style.times}>
             <div>
                 <p className={style.title}>Days</p>
-                <h1 className={style.number}>03</h1>
+                <h1 className={style.number}>{time.day}</h1>
             </div>
             <p className={style.twopoints}>:</p>
             <div>
                 <p className={style.title}>Hours</p>
-                <h1 className={style.number}>23</h1>
+                <h1 className={style.number}>{time.hours}</h1>
             </div>
             <p className={style.twopoints}>:</p>
             <div>
                 <p className={style.title}>Minutes</p>
-                <h1 className={style.number}>19</h1>
+                <h1 className={style.number}>{time.minutes}</h1>
             </div>
             <p className={style.twopoints}>:</p>
             <div>
                 <p className={style.title}>Seconds</p>
-                <h1 className={style.number}>56</h1>
+                <h1 className={style.number}>{time.seconds}</h1>
             </div>
         </div>
     )
